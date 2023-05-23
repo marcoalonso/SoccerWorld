7b73ba0c42561366ee2a114a4a296be76073a561
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-05-22 22:37:52 -0600

    Getting Started

---
+++ SoccerWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoccerWorld
+//
+//  Created by Marco Alonso Rodriguez on 22/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
